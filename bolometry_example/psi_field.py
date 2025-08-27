a56e471808af80699b44a9c0895f91eb5dd5ba34
C-bowman <chris.bowman.physics@gmail.com>
2025-08-27 14:08:34 +0100

    initial work on psi transformation and profile

---
+++ bolometry_example/psi_field.py
@@ -0,0 +1,79 @@
+from midas.fields import FieldModel
+from numpy import ndarray, zeros, diff
+from midas.parameters import FieldRequest, ParameterVector
+from abc import ABC, abstractmethod
+
+coordinates = dict[str, ndarray]
+
+
+class CoordinateTransform(ABC):
+    inputs: set[str]
+    outputs: set[str]
+
+    @abstractmethod
+    def __call__(self, input_coords: coordinates) -> coordinates:
+        pass
+
+
+class PsiTransform(CoordinateTransform):
+    def __init__(self, spline):
+        self.inputs = {"R", "z"}
+        self.outputs = {"psi"}
+        self.spline = spline
+
+
+class PiecewiseFluxProfile(FieldModel):
+    def __init__(
+        self,
+        field_name: str,
+        axis: ndarray,
+        axis_name: str,
+        psi_transform: CoordinateTransform
+    ):
+        assert axis.ndim == 1
+        assert axis.size > 1
+        assert (diff(axis) > 0.0).all()
+        assert axis.min() >= 0.
+        assert axis.max() <= 1.
+
+        self.name = field_name
+        self.n_params = axis.size
+        self.axis = axis
+        self.axis_name = axis_name
+        self.matrix_cache = {}
+        self.param_name = f"{field_name}_flux_profile_basis"
+        self.parameters = [ParameterVector(name=self.param_name, size=self.n_params)]
+        self.psi_transform = psi_transform
+
+    def get_basis(self, field: FieldRequest) -> ndarray:
+        if field in self.matrix_cache:
+            A = self.matrix_cache[field]
+
+        else:
+            psi_norm = self.psi_transform(field.coordinates)["psi"]
+            A = self.build_linear_basis(
+                x=psi_norm, knots=self.axis
+            )
+            self.matrix_cache[field] = A
+        return A
+
+    def get_values(
+        self, parameters: dict[str, ndarray], field: FieldRequest
+    ) -> ndarray:
+        basis = self.get_basis(field)
+        return basis @ parameters[self.param_name]
+
+    def get_values_and_jacobian(
+        self, parameters: dict[str, ndarray], field: FieldRequest
+    ) -> tuple[ndarray, dict[str, ndarray]]:
+        basis = self.get_basis(field)
+        return basis @ parameters[self.param_name], {self.param_name: basis}
+
+    @staticmethod
+    def build_linear_basis(x: ndarray, knots: ndarray) -> ndarray:
+        basis = zeros([x.size, knots.size])
+        for i in range(knots.size - 1):
+            k = ((x >= knots[i]) & (x <= knots[i + 1])).nonzero()
+            basis[k, i + 1] = (x[k] - knots[i]) / (knots[i + 1] - knots[i])
+            basis[k, i] = 1 - basis[k, i + 1]
+        return basis
---
+++ bolometry_example/psi_model.py
@@ -0,0 +1,90 @@
+from numpy import linspace, sin, cos, exp, log, sqrt, meshgrid, atan2, zeros
+import matplotlib.pyplot as plt
+
+from scipy.interpolate import CloughTocher2DInterpolator
+from tokamesh.tokamaks import mastu_boundary
+
+
+class ParametrisedPsi:
+    def __init__(
+        self,
+        r_min: float,
+        axis: tuple[float, float],
+        elongation=1.75,
+        triangularity=0.6
+    ):
+        self.axis = axis
+        self.r_min = r_min
+        self.kappa_max = elongation
+        self.delta_max = triangularity
+        self.psi_max = 1.05
+
+        n = 64
+        x = linspace(-2, 2, n)
+        y = linspace(-1.5, 1.5, n)
+        x_mesh, y_mesh = meshgrid(x, y, indexing="ij")
+        theta = atan2(y_mesh, x_mesh)
+        rho = sqrt(x_mesh ** 2 + y_mesh ** 2)
+
+        R, z = self.flux_contour(rho, theta)
+        psi_norm = self.psi(rho).flatten()
+
+        coords = zeros([R.size, 2])
+        coords[:, 0] = R.flatten()
+        coords[:, 1] = z.flatten()
+
+        self.spline = CloughTocher2DInterpolator(
+            points=coords,
+            values=psi_norm
+        )
+
+    def psi(self, rho):
+        sigma = sqrt(-0.5 / log((self.psi_max - 1) / self.psi_max))
+        return (1 - exp(-0.5*(rho / sigma)**2)) * self.psi_max
+
+    def shape_params(self, rho):
+        scale = 0.3
+        gaussian_ramp = 1 - exp(-0.5*(rho / scale)**2)
+        delta = gaussian_ramp * self.delta_max
+        kappa = 1 + gaussian_ramp * (self.kappa_max - 1)
+        return delta, kappa
+
+    def flux_contour(self, rho, theta):
+        R0, z0 = self.axis
+        delta, kappa = self.shape_params(rho)
+        R = rho * self.r_min * cos(theta + delta * sin(theta)) + R0
+        z = rho * self.r_min * kappa * sin(theta) + z0
+        return R, z
+
+
+
+if __name__ == "__main__":
+
+
+    psi = ParametrisedPsi(
+        r_min=0.5,
+        axis=(0.85, 0.01),
+        elongation=1.75,
+        triangularity=0.6,
+    )
+
+
+    R_axis = linspace(0.25, 1.5, 32)
+    z_axis = linspace(-1., 1., 32)
+    R_mesh, z_mesh = meshgrid(R_axis, z_axis, indexing="ij")
+
+
+    points = zeros([R_mesh.size, 2])
+    points[:, 0] = R_mesh.flatten()
+    points[:, 1] = z_mesh.flatten()
+
+
+    psi_interp = psi.spline(points)
+    psi_interp.resize(R_mesh.shape)
+
+
+    plt.contourf(R_axis, z_axis, psi_interp.T, 32)
+    plt.contour(R_axis, z_axis, psi_interp.T, levels=[1.0], colors=["red"])
+    plt.plot(*mastu_boundary(), lw=2, color="black")
+    plt.axis("equal")
+    plt.show()
